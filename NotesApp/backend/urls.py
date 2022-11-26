9202d12e8208a2267ed581dc4e4f67cb39463f8d
fhraju <alonghuman@gmail.com>
2022-11-27 00:18:09 +0600

    full project

--- NotesApp/backend/urls.py
+++ NotesApp/backend/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes'),
+    path('', views.getRoutes, name='routes'),    
     path('notes/', views.getNotes, name='notes'),
+    path('notes/new', views.createNote, name='create-note'),
+    path('notes/<str:pk>/update', views.updateNote, name='update-note'),
+    path('notes/<str:pk>/delete', views.deleteNote, name='delete-note'),
     path('notes/<str:pk>', views.getNote, name='note'),
 ]
--- NotesApp/backend/views.py
+++ NotesApp/backend/views.py
@@ -43,8 +43,16 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getNotes(request):
-    notes = Note.objects.all()
+    notes = Note.objects.all().order_by('-updated_at')
     serializer = NoteSerializer(notes, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(body = data['body'])
+    serializer = NoteSerializer(note, many= False)
 
     return Response(serializer.data)
 
@@ -54,3 +62,20 @@ def getNote(request, pk):
     serializer = NoteSerializer(note, many=False)
 
     return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+    
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response("The note was deleted")
